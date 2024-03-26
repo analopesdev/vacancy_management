af533d18d24fa49d8aca1cb995f4f40b561a1e8f
Ana Lopes <analopesdemorais@outlook.com>
2024-03-26 19:33:18 -0300

    initial commit

---
+++ src/test/java/br/com/analopesdev/vacancy_management/modules/candidate/controllers/CandidateController.java
@@ -0,0 +1,13 @@
+package br.com.analopesdev.vacancy_management.modules.candidate.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/candidates")
+public class CandidateController {
+  public void create(){
+    
+  }
+  
+}
