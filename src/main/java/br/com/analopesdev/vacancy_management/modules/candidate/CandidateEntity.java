0e6f22df724503151460141d81749bca1c314943
Ana Lopes <analopesdemorais@outlook.com>
2024-03-26 20:03:18 -0300

    feat: create entity candidate and controller

---
+++ src/main/java/br/com/analopesdev/vacancy_management/modules/candidate/CandidateEntity.java
@@ -0,0 +1,29 @@
+package br.com.analopesdev.vacancy_management.modules.candidate;
+
+import java.util.UUID;
+
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+public class CandidateEntity {
+  private UUID id;
+  private String name;
+
+  @Pattern(regexp = "^(?!\\s*$).*", message = "Username should not be empty")
+  private String username;
+
+  @Email(message = "Email should be valid")
+  private String email;
+
+  @Length(min = 10, max = 100)
+  private String password;
+
+  @Length(min = 10, max = 255)
+  private String description;
+
+  private String curriculum;
+}
