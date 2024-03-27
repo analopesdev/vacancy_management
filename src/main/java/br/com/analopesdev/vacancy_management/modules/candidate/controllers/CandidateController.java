2ef8fe993b638944a0f761959b8166c2ecf5a520
Ana Lopes <analopesdemorais@outlook.com>
2024-03-27 09:12:38 -0300

    feat: config jpa and create repository

--- src/main/java/br/com/analopesdev/vacancy_management/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/analopesdev/vacancy_management/modules/candidate/controllers/CandidateController.java
@@ -1,19 +1,23 @@
 package br.com.analopesdev.vacancy_management.modules.candidate.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.analopesdev.vacancy_management.modules.candidate.CandidateEntity;
+import br.com.analopesdev.vacancy_management.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid; 
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
+  @Autowired
+  private CandidateRepository candidateRepository;
 
   @PostMapping("/")
-  public void create(@Valid @RequestBody CandidateEntity candidateEntity){
-     System.out.println(candidateEntity.getEmail());
+  public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity){
+    return this.candidateRepository.save(candidateEntity);
   }
 }
