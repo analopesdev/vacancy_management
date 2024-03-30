f113f5e28d3063cb9a65fea156367d45cbc9d86e
Ana Lopes <analopesdemorais@outlook.com>
2024-03-30 08:31:10 -0300

    feat: configure swagger api

--- src/main/java/br/com/analopesdev/vacancy_management/VacancyManagementApplication.java
+++ src/main/java/br/com/analopesdev/vacancy_management/VacancyManagementApplication.java
@@ -1,9 +1,18 @@
 package br.com.analopesdev.vacancy_management;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+  info = @Info(
+    title = "Vacancy Management",
+    version = "1.0.0",
+    description = "API for managing vacancies"
+  )
+)
 public class VacancyManagementApplication {
 
   public static void main(String[] args) {
